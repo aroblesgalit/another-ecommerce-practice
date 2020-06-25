0685dbe28df9e60ed9ee77eacf23e34c010e8766
Alvin Galit <aroblesgalit@gmail.com>
2020-06-24 21:28:56 -0500

    Cleaned up ProductList

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-    
-    state = {
-        products: storeProducts
-    };
-
     render() {
-        
-        // console.log(this.state.products);
-
         return (
             <React.Fragment>
                 <div className="py-5">
@@ -33,7 +24,6 @@ export default class ProductList extends Component {
                     </div>
                 </div>
             </React.Fragment>
-            // <Product />
         )
     }
 }
