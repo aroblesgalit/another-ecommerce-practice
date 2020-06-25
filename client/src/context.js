081f6aff48bb1f4da2d3c561f2f5296c0ebc370a
Alvin Galit <aroblesgalit@gmail.com>
2020-06-25 08:31:05 -0500

    Created setProducts method to create a new array from the data and run it in componentDidMount

--- client/src/context.js
+++ client/src/context.js
@@ -8,9 +8,24 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
 
     state = {
-        products: storeProducts,
+        products: [],
         detailProduct
     };
+
+    componentDidMount() {
+        this.setProducts();
+    }
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = { ...item };
+            tempProducts = [...tempProducts, singleItem];
+        });
+        this.setState(() => {
+            return { products: tempProducts }
+        });
+    }
 
     handleDetail = () => {
         console.log("hello from detail");
@@ -19,7 +34,22 @@ class ProductProvider extends Component {
     addToCart = () => {
         console.log("hello from add to cart");
     };
-    
+
+    // Test 
+    // tester = () => {
+    //     console.log("State products: ", this.state.products[0].inCart);
+    //     console.log("Data products: ", storeProducts[0].inCart);
+
+    //     const tempProducts = [...this.state.products];
+    //     tempProducts[0].inCart = true;
+    //     this.setState(() => {
+    //         return { products: tempProducts }
+    //     }, () => {
+    //         console.log("State products: ", this.state.products[0].inCart);
+    //         console.log("Data products: ", storeProducts[0].inCart);
+    //     })
+    // }
+
     render() {
         return (
             <ProductContext.Provider value={{
